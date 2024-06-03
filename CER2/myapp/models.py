29a6a909b2a8063b4a8b55c9ea47aa9fb26e957b
ASHLEYIB <ashley.iturriaga@usm.cl>
2024-06-02 21:19:44 -0400

    proyectos

--- CER2/myapp/models.py
+++ CER2/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    nombreProyecto = models.CharField(max_length=78)
+    estudiante = models.CharField(max_length=200)
+    tema = models.CharField(max_length=100)
+    profesor = models.CharField(max_length=300)
+    def __str__(self) -> str:
+        return self.nombreProyecto
--- CER2/myapp/views.py
+++ CER2/myapp/views.py
@@ -1,5 +1,12 @@
+from .models import *
 from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    return render(request,'myapp/base.html')
+    datos = {}
+    proyectos = Proyecto.objects.all()
+    nombres = []
+    for proyecto in proyectos:
+        nombres.append(proyecto.nombreProyecto)
+    datos["nombreProyecto"] = nombres
+    return render(request,'myapp/base.html', datos)
